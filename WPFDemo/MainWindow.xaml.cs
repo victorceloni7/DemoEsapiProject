90aaeb82746767f23e3b816ad315efdadac3f2b1
Victor Celoni <victorceloni@hotmail.com>
2023-04-21 16:02:27 -0300

    Commit

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -1,62 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace WPFDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
+
     public partial class MainWindow : Window
     {
-        public List<Tratamento> tipotratamento = new List<Tratamento>(); //Criando 
+        //Criando Lista de tratamentos
+        public List<Tratamento> tipotratamento = new List<Tratamento>();
+        public List<OAR> oars = new List<OAR>();
+        public List<Constraints> constraints = new List<Constraints>();
         public MainWindow()
         {
-            InitializeComponent();
-            tipotratamento.Add(new Tratamento { Regiao = "Mama sem fossa", Tipo = "UK START B", FracDose = "15fr x 266,7cGy (4000cGy)" });
-            tipotratamento.Add(new Tratamento { Regiao = "Mama com fossa", Tipo = "UK START B", FracDose = "15fr x 266,7cGy (4000cGy)" });
-            tipotratamento.Add(new Tratamento { Regiao = "Mama", Tipo = "FAST FORWARD LYMPHATIC RT", FracDose = "5fr x 520cGy (2600cGy)" });
-            tipotratamento.Add(new Tratamento { Regiao = "Mama", Tipo = "FAST FORWARD", FracDose = "5fr x 520cGy (2600cGy)" });
+            InitializeComponent(); //Inicializando a seleção de tratamentos 
 
-            ComboTratamento.ItemsSource = tipotratamento;
+            tipotratamento.Add(new Tratamento { NomeTratamento = "Mama sem fossa UK START B 15fr x 266,7cGy (4000cGy)"});   // [0]
+
+            tipotratamento.Add(new Tratamento { NomeTratamento = "Mama com fossa UK START B 15fr x 266,7cGy (4000cGy)" });  // [1]
+
+            tipotratamento.Add(new Tratamento { NomeTratamento = "Mama FAST FORWARD LYMPHATIC RT 5fr x 520cGy (2600cGy)" });// [2]
+
+
+
+            tipotratamento.Add(new Tratamento { NomeTratamento = "Mama FAST FORWARD 5fr x 520cGy (2600cGy)" });             // [3] 
+
+                oars.Add(new OAR { NomeOar = "Coracao" });     // [0]
+
+                    constraints.Add(new Constraints { NomeC = "V150cGy", ValorC = 30, ValorAceitavelC = 30 });
+                    constraints.Add(new Constraints { NomeC = "V700cGy", ValorC = 5, ValorAceitavelC = 5 });
+
+                oars.Add(new OAR { NomeOar = "Pulmao_esq" });  // [1]
+                    constraints.Add(new Constraints { NomeC = "800", ValorC = 25, ValorAceitavelC = 25 });
+
+                oars.Add(new OAR { NomeOar = "Pulmao_dir" });  // [2]
+                oars.Add(new OAR { NomeOar = "Mama contra" }); // [3]
+
+            ComboTratamento.ItemsSource = tipotratamento; //enviando os tipos de tratamentos para a seletor ComboBox
+                        
+            oars.Add(new OAR { NomeOar = "Coracao" });     // [0]
+            oars.Add(new OAR { NomeOar = "Pulmao_esq" });  // [1]
+            oars.Add(new OAR { NomeOar = "Pulmao_dir" });  // [2]
+            oars.Add(new OAR { NomeOar = "Mama contra" }); // [3]
+
+            
+          
+            
+
         }
 
-        private void VerificaDVH_Click(object sender, RoutedEventArgs e)
+        private void VerificaDVH_Click(object sender, RoutedEventArgs e)//Configurando botão para verificar DVH
         {
-            MessageBox.Show($"Hello {tipotratamento[0].Tipo}");
-        }
-    }
-
-    public class Tratamento
-    {
-        public string Regiao { get; set; } = string.Empty;
-        public string Tipo { get; set; } = string.Empty;
-        public string FracDose { get; set; } = string.Empty;
-        public string NomeTratamento
-        {
-            get
+            MessageBox.Show($"Hello {tipotratamento[ComboTratamento.SelectedIndex].NomeTratamento}"); //Atual: Mostra o nome do tratamento selecionado. Futuro: Substituir por comparações no futuro
+            if (tipotratamento[ComboTratamento.SelectedIndex].NomeTratamento.ToString() == "Mama sem fossa UK START B 15fr x 266,7cGy (4000cGy)")
             {
-                return $"{Regiao} {Tipo} {FracDose}";
+                MessageBox.Show("Voce selecionou correto!");
+
             }
+            else
+                MessageBox.Show("Voce selecionou errado!");
         }
     }
-    public class OAR
+
+    public class Tratamento //Classe Tratamento (define o nome contido na ficha de tratamento)
+    {
+        public string NomeTratamento { get;set; } = string.Empty;
+        
+    }
+    public class OAR : Tratamento //Classe de orgão de risco -> Cada tratamento possui uma lista de OARs
     {
         public string NomeOar { get; set; } = string.Empty;
-        public string Constraint { get; set; } = string.Empty;
-        public string NomeTratamento { get; set; } = string.Empty;
+
     }
+
+    public class Constraints : OAR //Classe de constraints para cada orgão de risco
+    {
+        public string NomeC { get; set; } = string.Empty;
+        public int ValorC { get; set; } = 0;
+        public int ValorAceitavelC { get; set; } = 0;
+    }
+
 }
 
